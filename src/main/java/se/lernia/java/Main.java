43d478f298b8b0fe5be13bf79a318a4df1731bec
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-20 15:12:14 +0200

    Find a in string

--- src/main/java/se/lernia/java/Main.java
+++ src/main/java/se/lernia/java/Main.java
@@ -5,58 +5,21 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
-        int result =max(10, 5);
-        System.out.println(result);
-
-        float averageValue = average(15.4f, 4.3f);
-        System.out.println(averageValue);
-
-        averageValue = average(15.4f, 4.3f, 1.0f);
-        System.out.println(averageValue);
-
-        float[] values =  {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
-        System.out.println(average(values));
+        String s = "Heja";
+        System.out.println(letterCounter(s));
     }
 
-    static int max(int tal1, int tal2) {
-        if (tal1 > tal2) {
-            return tal1;
+    public static int letterCounter(String word) {
+        int count = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i)== 'a') {
+                count++;
+            }
         }
-        return tal2;
+        return count;
     }
-
-    static float average(int number1, int number2){
-        return (number1 + number2) / 2.0f;
-    }
-    static float average(float number1, float number2){
-        return (number1 + number2) / 2;
-    }
-    //This is method overloading, multiple methods with same name but different parameters.
-    static float average(float number1, float number2, float number3){
-        return (number1 + number2 + number3) / 3;
-    }
-
-//    //varargs
-//    static float average(float... numbers){
-//        float sum = 0;
-//        for (int i = 0; i < numbers.length; i++) {
-//            sum += numbers[i];
-//        }
-//        return sum / numbers.length;
-//    }
-
-    static float average(float[] numbers) {
-        float sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
-        return sum / numbers.length;
-    }
-
 }
-
-//Todo: Skriv en metod som tar MÅNGA värden och returnerar medelvärdet... (Olika sätt att lösa det på finns)
 
 //Todo: Skriv en metod som tar en String som inparameter och returnerar hur många a det finns
 //Todo: Skriv en metod som tar två String som inparametrar och returnerar true eller false om de är lika eller olika
