5d6619d68bc486e1065984884043eb0c09ff3b71
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-20 12:52:53 +0200

    Add todo for exercises

---
+++ src/main/java/se/lernia/java/Main.java
@@ -0,0 +1,28 @@
+package se.lernia.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+//Todo: Skriv en applikation som skriver ut talen 10,20,30… upp till 100.
+//Todo: Skriv en applikation som med utskrift räknar ner baklänges från 10 till 0.
+//Todo: Skriv en applikation som skriver ut alfabetet a,b,c…
+
+//Todo: Skriv en metod som tar 2 heltal som inparametrar och returnerar det största talet
+//Todo: Skriv en metod som tar 2 float som inparametrar och returnerar medelvärdet
+//Todo: Skriv en metod som tar 3 float som inparametrar och returnerar medelvärdet
+//Todo: Skriv en metod som tar MÅNGA värden och returnerar medelvärdet... (Olika sätt att lösa det på finns)
+
+//Todo: Skriv en metod som tar en String som inparameter och returnerar hur många a det finns
+//Todo: Skriv en metod som tar två String som inparametrar och returnerar true eller false om de är lika eller olika
+//Todo: Skapa en metod som ovan men som ignorerar case vid jämförelsen
+
+//Todo: Skapa en metod som tar en string innehållande siffror som inparameter och tar bort inledande nollor. Input: 00000123569 Output : 123569
+//Todo: Skapa en metod som returnerar en String reversed
+//Todo: Skapa en metod som returnerar true om en String är ett palindrom
