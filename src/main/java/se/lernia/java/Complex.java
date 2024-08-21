90c7a5f596af0e5beb3803d7feaee46c13b16de3
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-21 11:31:23 +0200

    Add reading from keyboard with Scanner object

--- src/main/java/se/lernia/java/Complex.java
+++ src/main/java/se/lernia/java/Complex.java
@@ -1,6 +1,12 @@
 package se.lernia.java;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Complex {
+
+    //This scanner object lives during the whole application and is used for reading from keyboard.
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         stackOverflow();
@@ -10,11 +16,29 @@ public class Complex {
         int scorePlayer1 = 0;
         int scorePlayer2 = 0;
 
-        Player player1 = new Player("Martin", 0);
+        //Fråga användaren efter namn?
+        System.out.print("Please enter your name ");
+        String name = sc.nextLine();
+        System.out.println("Please enter your starting score");
+        int startingScore = 0;
+        try {
+            startingScore = sc.nextInt();
+        } catch (InputMismatchException e) {
+            //Handle invalid inputs here
+            System.out.println("You can't do that. You will get zero as score");
+        }
+        sc.nextLine(); //To remove trailing return after nextInt()
+
+        Player player1 = new Player(name, startingScore);
         player1.increaseScore();
-        System.out.println(player1.getName() + " has score " +player1.getScore());
+        System.out.println(player1.getName() + " has score " + player1.getScore());
+        System.out.printf("%s has score %d\n", player1.getName(), player1.getScore());
 
-        Player player2 = new Player();
-
+        System.out.println("Please enter player twos name");
+        String player2Name = sc.nextLine();
+        System.out.println("You entered player twos name as " + player2Name);
+        System.out.println("Please enter starting score for player 2");
     }
+
+
 }
