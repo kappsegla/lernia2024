e9519cad5bf9764294f4b4fde6f28512d1d8d776
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-28 12:59:08 +0200

    Start EventLog example implementation

---
+++ src/main/java/se/lernia/java/records/EventLog.java
@@ -0,0 +1,24 @@
+package se.lernia.java.records;
+
+//Todo: Implement an event log
+// - A method can be called to store an event that happened, use a Record for representing an event.
+// - Each event should store type of event, who/what triggered the even and a timestamp
+// - Use the generic ArrayList<Event> for storing the events
+
+//Todo: Implement a method or methods that can find and return all events from a specified time.
+// - Specific day or hour
+// - Intervall that could be from 8.00 a day to 12.00 for example.
+// - What should we return when not finding any events?
+// - What should happen if we specify start time that is after endtime?
+
+//Todo: Implement a method that can visually present the events depending on the type of event
+// - We want to use switch to check for different event types.
+// - What type should event type be?
+
+
+public class EventLog {
+
+    public static void main(String[] args) {
+
+    }
+}
