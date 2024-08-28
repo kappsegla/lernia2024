0db487526fd72b7fb56cff83db0952607e1ca88f
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-28 13:52:03 +0200

    Add implementation for event log

--- src/main/java/se/lernia/java/records/EventLog.java
+++ src/main/java/se/lernia/java/records/EventLog.java
@@ -1,9 +1,12 @@
 package se.lernia.java.records;
 
 //Todo: Implement an event log
-// - A method can be called to store an event that happened, use a Record for representing an event.
+// - A method can be called to store an event that happened, use a record for representing an event.
 // - Each event should store type of event, who/what triggered the even and a timestamp
 // - Use the generic ArrayList<Event> for storing the events
+// - Event types?
+// - Who? Id? Name?
+// - TimeStamp milliseconds since EPOCH? LocalDateTime?
 
 //Todo: Implement a method or methods that can find and return all events from a specified time.
 // - Specific day or hour
@@ -16,9 +19,69 @@ package se.lernia.java.records;
 // - What type should event type be?
 
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class EventLog {
 
-    public static void main(String[] args) {
+    static ArrayList<Event> events = new ArrayList<>();
 
+    public static void main(String[] args) {
+        storeEvent("Error", "1234");
+        storeEvent("Warning", "1234");
+        storeEvent("Break", "999");
+
+        events.forEach(System.out::println);
     }
+
+    private static void storeEvent(String type, String id) {
+        Event e = new Event(new EventType(type), new EventTrigger(id), LocalDateTime.now());
+        events.add(e);
+    }
+
 }
+
+record Event(EventType type, EventTrigger trigger, LocalDateTime time) {
+}
+
+record EventType(String name) {
+}
+
+record EventTrigger(String triggerId) {
+}
+
+//final class NotFullyImmutable {
+//    private final ArrayList<String> strings;
+//
+//    NotFullyImmutable(ArrayList<String> strings) {
+//        this.strings = new ArrayList<>(strings);
+//    }
+//
+//    public List<String> strings() {
+//        return Collections.unmodifiableList(strings);
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == this) return true;
+//        if (obj == null || obj.getClass() != this.getClass()) return false;
+//        var that = (NotFullyImmutable) obj;
+//        return Objects.equals(this.strings, that.strings);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(strings);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "NotFullyImmutable[" +
+//               "strings=" + strings + ']';
+//    }
+//
+//
+//}
