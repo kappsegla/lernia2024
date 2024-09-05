9791e8dde0429f39555baa94ae1e1f799aa0b814
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-05 13:29:22 +0200

    Add search for employees with salary over 2 mil

--- src/main/java/se/lernia/java/streams/App.java
+++ src/main/java/se/lernia/java/streams/App.java
@@ -1,7 +1,26 @@
 package se.lernia.java.streams;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        var employeeFactory = new EmployeeFactory();
+        var employees = employeeFactory.getAllEmployees();
 
+        employeesWithSalaryOver2Mil(employees);
+
+
+
+    }
+
+    private static void employeesWithSalaryOver2Mil(List<Employee> employees) {
+        //Find all employees with salary greater than a certain amount (Use filter and forEach methods
+        employees.stream()
+                .filter(App::salaryOver2Mil)
+                .forEach(System.out::println);
+    }
+
+    private static boolean salaryOver2Mil(Employee employee) {
+        return employee.salary() > 2_000_000;
     }
 }
--- src/main/java/se/lernia/java/streams/Employee.java
+++ src/main/java/se/lernia/java/streams/Employee.java
@@ -4,4 +4,7 @@ import java.util.List;
 
 public record Employee(String id, String firstName, String lastName, int salary, int totalLaptopsAssigned,
                        List<Project> projects) {
+    public String fullName() {
+        return firstName() + " " + lastName();
+    }
 }
