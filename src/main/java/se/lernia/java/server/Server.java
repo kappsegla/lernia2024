14d524980a378ed6354f5d151389189c96567d3c
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-09 13:52:27 +0200

    Add response to client

--- src/main/java/se/lernia/java/server/Server.java
+++ src/main/java/se/lernia/java/server/Server.java
@@ -1,10 +1,13 @@
 package se.lernia.java.server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class Server {
+
+    public final Logger log = Logger.getLogger(Server.class.getName());
 
     private final int port;
 
@@ -23,18 +26,34 @@ public class Server {
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
+            log.info("Server listening on port " + port);
             while (true) {
                 Socket socket = serverSocket.accept();
-                //Todo: handle client connection in socket object
-                System.out.println("Client connected");
-                socket.close();
+                handleClient(socket);
             }
         } catch (IOException e) {
 
         }
-
-
     }
 
-
+    public void handleClient(Socket socket) {
+        log.info("Client connected from " + socket.getRemoteSocketAddress());
+        try (Socket clientSocket = socket) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+            //Todo: handle client send and receive
+            //Read http request
+            //Send response
+            String response = """
+                    HTTP/1.1 200 OK
+                    Content-Type: text/plain
+                    Content-Length: 11
+                    
+                    Hello World
+                    """;
+            out.print(response);
+            out.flush();
+        } catch (IOException e) {
+        }
+    }
 }
