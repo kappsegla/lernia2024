7576fd239e46bc2947e5e7ed9a24cac9ee905cec
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-09 11:52:19 +0200

    Add more tests

--- src/test/java/se/lernia/java/functional/PureTest.java
+++ src/test/java/se/lernia/java/functional/PureTest.java
@@ -1,10 +1,17 @@
 package se.lernia.java.functional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PureTest {
@@ -12,7 +19,7 @@ class PureTest {
     @Test
     @DisplayName("Add method with params 3 and 5 should return 5 👌")
     void addTwoAndThreeReturnsFive() {
-        var result = Pure.add(2,3);
+        var result = Pure.add(2, 3);
 
         assertEquals(5, result, "Addition of 2 and 3 failed");
     }
@@ -20,7 +27,7 @@ class PureTest {
     @Test
     @DisplayName("Greet with good morning before 12")
     void greetWithGoodMorningBefore12() {
-        var time = LocalTime.of(11,59);
+        var time = LocalTime.of(11, 59);
 
         var result = Pure.greeting("Martin", time);
 
@@ -30,9 +37,31 @@ class PureTest {
     @Test
     @DisplayName("Greet with good afternoon after 12")
     void greetWithGoodAfternoonAfter12() {
-        assertEquals("Good afternoon Martin", Pure.greeting("Martin", LocalTime.of(12,0)));
+        assertEquals("Good afternoon Martin", Pure.greeting("Martin", LocalTime.of(12, 0)));
     }
 
+    @Test
+    @DisplayName("Greet with good afternoon after 15")
+    void greetWithGoodAfternoonAfter15() {
+        assertThat(Pure.greeting("Kalle", LocalTime.of(15, 0)))
+                .startsWith("Good")
+                .contains("Kalle")
+                .hasSizeLessThan(50);
+    }
 
+    @ParameterizedTest(name = "summing {0} and {1} should give {2}")
+    @MethodSource("integersProvider")
+    void shouldSumTwoIntegers(int x, int y, int expectedResult) {
+        //when
+        int result = Pure.add(x, y);
+        //expect
+        assertEquals(expectedResult, result);
+    }
 
+    private static Stream<Arguments> integersProvider() {
+        return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(-1, 2, 1),
+                Arguments.of(3, 0, 3));
+    }
 }
