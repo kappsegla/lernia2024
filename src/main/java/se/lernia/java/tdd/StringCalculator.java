2d77c041b62276036469722c15464753006dc4c8
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-11 10:14:38 +0200

    Handle multiple numbers with \n and , as separators

--- src/main/java/se/lernia/java/tdd/StringCalculator.java
+++ src/main/java/se/lernia/java/tdd/StringCalculator.java
@@ -1,9 +1,14 @@
 package se.lernia.java.tdd;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public static int add(String s) {
         if( s.isEmpty() )
             return 0;
-        return Integer.parseInt(s);
+
+        return Arrays.stream(s.split("[,\n]"))
+                .mapToInt(Integer::valueOf)
+                .sum();
     }
 }
--- src/test/java/se/lernia/java/tdd/StringCalculatorTest.java
+++ src/test/java/se/lernia/java/tdd/StringCalculatorTest.java
@@ -16,4 +16,15 @@ public class StringCalculatorTest {
     void givenStringWith1WhenCallingAddThenReturn1() {
         assertThat(StringCalculator.add("1")).isEqualTo(1);
     }
+
+    @Test
+    void givenStringWith2ValuesSeparatedByCommaWhenCallingAddThenReturnSum() {
+        assertThat(StringCalculator.add("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void givenStringWithMultipleValuesSeparatedByCommaAndNewLineCallingAddThenReturnSum() {
+        assertThat(StringCalculator.add("1\n2,3")).isEqualTo(6);
+    }
+
 }
