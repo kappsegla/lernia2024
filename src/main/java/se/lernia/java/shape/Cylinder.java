8e27b6602bab96b1d50495ee35f7f05aa7825a6e
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-18 18:51:25 +0200

    Change Cylinder to using Composition instead of Inheritance

--- src/main/java/se/lernia/java/shape/Cylinder.java
+++ src/main/java/se/lernia/java/shape/Cylinder.java
@@ -2,6 +2,7 @@ package se.lernia.java.shape;
 
 import java.awt.*;
 
-public class Cylinder extends Circle {
+public class Cylinder{
+    Circle circle;
     float height;
 }
--- src/main/java/se/lernia/java/shape/Shape.java
+++ src/main/java/se/lernia/java/shape/Shape.java
@@ -5,17 +5,11 @@ public class Shape {
 
     public static void main(String[] args) {
         Circle circle = new Circle();
-        Circle circle2 = new Cylinder();
         Cylinder cylinder = new Cylinder();
 
-        circle.radius = 1;
-        circle2.radius = 1;
-        cylinder.radius = 1;
         cylinder.height = 10;
 
         Object o = new Circle();
         Object o2 = new Cylinder();
-
-
     }
 }
